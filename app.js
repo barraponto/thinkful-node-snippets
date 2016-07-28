b69d4f35db529ace3221ecd966f069c038d7bc23
Capi Etheriel <barraponto@gmail.com>
2016-07-28 10:15:06 -0300

    mongo crud

--- app.js
+++ app.js
@@ -4,10 +4,42 @@ MongoClient.connect('mongodb://localhost/', function(error, db){
   if (error) { console.error(error); db.close(); return; }
   var collection = db.collection('snippets');
 
-  var create = function(name, content) { db.close(); };
-  var read = function(name) { db.close(); };
-  var update = function(name, content) { db.close(); };
-  var delete_ = function(name) { db.close(); };
+  var create = function(name, content) {
+    var snippet = {name: name, content: content};
+    collection.insert(snippet, function(error, result){
+      if (error) { console.error(error); db.close(); return; }
+      console.log('Created snippet', name);
+      db.close();
+    });
+  };
+  var read = function(name) {
+    var query = {name: name};
+    collection.findOne(query, function(error, snippet){
+      if (error || !snippet) { console.error('Could not read snippet ', name); db.close(); return; }
+      console.log('Read snippet', name);
+      console.log(snippet.content);
+      db.close();
+    });
+  };
+  var update = function(name, content) {
+    var query = {name: name};
+    var update = {$set: {content: content}};
+    collection.findAndModify(query, null, update, function(error, result){
+      var snippet = result.value;
+      if (error || !snippet) { console.error('Could not update snippet ', name); db.close(); return; }
+      console.log('Updated snippet', name);
+      db.close();
+    });
+  };
+  var delete_ = function(name) {
+    var query = {name: name};
+    collection.findAndRemove(query, function(error, result){
+      var snippet = result.value;
+      if (error || !snippet) { console.error('Could not delete snippet ', name); db.close(); return; }
+      console.log('Deleted snippet', name);
+      db.close();
+    });
+  };
 
   var main = function(){
     var command = process.argv[2];
